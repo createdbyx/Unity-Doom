89ab37bce4e2ff1fa9a8a5c357d7946231d79347
Rellac <dannycaller@gmail.com>
2018-08-18 17:50:55 +0100

    basic audio

---
+++ Assets/DoomGame/Scripts/EntityAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAudio : MonoBehaviour {
+
+	[SerializeField] private SoundInput[] sounds = null;
+
+	private Dictionary<string, AudioClip[]> soundDict = new Dictionary<string, AudioClip[]>();
+	private AudioSource audioSource;
+
+	private void Awake ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		foreach (SoundInput si in sounds) {
+			if (soundDict.ContainsKey (si.name)) {
+				Debug.LogError ("Tried to Add Duplicate Sound ID: " + si.name);
+				continue;
+			} else if (si.soundIds.Length > 0) {
+				AudioClip[] clips = new AudioClip[si.soundIds.Length];
+				for (int i = 0; i < si.soundIds.Length; i++) {
+					clips[i] = SoundLoader.LoadSound(si.soundIds[i]);
+				}
+				soundDict.Add(si.name, clips);
+				continue;
+			}
+			Debug.LogError("No IDs supplied for Sound: " + si.name);
+		}
+	}
+
+	public void PlaySound(string soundID) {
+		AudioClip[] clips;
+		if (soundDict.TryGetValue (soundID, out clips)) {
+			audioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+			return;
+		}
+		Debug.LogError ("Tried to play unloaded Sound: " + soundID);
+	}
+
+	[System.Serializable]
+	private struct SoundInput {
+		public string name;
+		public string[] soundIds;
+	}
+}

8fa89161ad19fc4db07d2aae0a7dcaa8fd149caf
Rellac <dannycaller@gmail.com>
2018-08-19 17:32:38 +0100

    major changes to ammo system to fit well with multi-ammo menu

--- Assets/DoomGame/Scripts/WeaponManager.cs
+++ Assets/DoomGame/Scripts/WeaponManager.cs
@@ -11,27 +11,31 @@ public class WeaponManager : MonoBehaviour {
 	[SerializeField] private GUIManager guiManager = null;
 	[SerializeField] private AudioSource audioSource = null;
 	[SerializeField] private LayerMask entityLayer = 0;
+	[SerializeField] private AmmoConfig[] ammo = null;
 	[SerializeField] private WeaponType[] weaponTypes = null;
+
+	private Dictionary<AmmoType, AmmoConfig> ammoDict = new Dictionary<AmmoType, AmmoConfig>();
 
 	private int weaponIdx = 0;
 	private int animIdx = 0;
 
 	public int CurrentAmmo {
 		get {
-			return (weaponTypes[weaponIdx].unlimitedAmmo ? -1 : weaponTypes[weaponIdx].ammo);
+			return (weaponTypes[weaponIdx].ammoType == AmmoType.UNLM ? -1 : GetAmmo(weaponTypes[weaponIdx].name));
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		Invoke("Init", 0); // wait a frame to be sure that the texture loader has initialised
+		Invoke("InitAmmo", 0); // wait a frame to be sure that the texture loader has initialised
+		Invoke("InitWeapon", 0); // wait a frame to be sure that the texture loader has initialised
 	}
 
 	void Update ()
 	{
 		// Shoot
 		if (Input.GetMouseButtonDown (0)) {
-			if (animIdx == 0 && (weaponTypes [weaponIdx].ammo != 0 || weaponTypes [weaponIdx].unlimitedAmmo)) {
+			if (animIdx == 0 && (GetAmmo(weaponTypes[weaponIdx].name) != 0 || weaponTypes [weaponIdx].ammoType == AmmoType.UNLM)) {
 				PlayShootAnimation ();
 			}
 		}
@@ -71,9 +75,9 @@ public class WeaponManager : MonoBehaviour {
 			}
 
 			if (animIdx == weaponTypes [weaponIdx].attackIdx) {
-				if (!weaponTypes [weaponIdx].unlimitedAmmo) {
-					weaponTypes [weaponIdx].ammo--;
-					guiManager.SetAmmo(weaponTypes [weaponIdx].ammo);
+				if (weaponTypes [weaponIdx].ammoType != AmmoType.UNLM) {
+					SetAmmo(weaponTypes[weaponIdx].name, GetAmmo(weaponTypes[weaponIdx].name) - 1);
+					guiManager.SetAmmo(GetAmmo(weaponTypes[weaponIdx].name));
 				}
 				switch (weaponTypes[weaponIdx].attackType) {
 				case AttackType.MeleeOneShot:
@@ -93,7 +97,19 @@ public class WeaponManager : MonoBehaviour {
 		}
 	}
 
-	private void Init() {
+	private void InitAmmo ()
+	{
+		foreach (AmmoConfig ac in ammo) {
+			if (!ammoDict.ContainsKey (ac.ammoType)) {
+				ac.UpdateAmmoDisplay();
+				ammoDict.Add(ac.ammoType, ac);
+				continue;
+			}
+			Debug.LogError("Already contains definition for AmmoType: " + ac.ammoType.ToString());
+		}
+	}
+
+	private void InitWeapon() {
 		SetSelectedWeapon(startWeapon);
 	}
 
@@ -116,6 +132,7 @@ public class WeaponManager : MonoBehaviour {
 		weaponIdx = idx;
 		SetWeaponSprite(weaponTypes [weaponIdx].idleSprite);
 		weaponTypes[weaponIdx].weaponImage.gameObject.SetActive(true);
+		guiManager.SetAmmo(GetAmmo(weaponTypes[weaponIdx].name));
 	}
 
 	public void SetSelectedWeapon (string id)
@@ -135,11 +152,36 @@ public class WeaponManager : MonoBehaviour {
 		weaponTypes[weaponIdx].weaponImage.texture = t;
 	}
 
+	private int GetAmmo (string weaponID)
+	{
+		foreach (WeaponType wt in weaponTypes) {
+			if (wt.name == weaponID) {
+				AmmoConfig outConf;
+				if (ammoDict.TryGetValue (wt.ammoType, out outConf)) {
+					return outConf.currentAmmo;
+				}
+				if (wt.ammoType != AmmoType.UNLM) {
+					Debug.LogError ("Weapon: " + wt.name + " using unlisted ammo type: " + wt.ammoType.ToString ());
+				}
+				return 0;
+			}
+		}
+		Debug.LogError("Invalid Weapon ID: " + weaponID);
+		return 0;
+	}
+
 	public void SetAmmo (string weaponID, int ammo)
 	{
 		foreach (WeaponType wt in weaponTypes) {
 			if (wt.name == weaponID) {
-				wt.ammo = ammo;
+				//wt.ammo = ammo;
+				AmmoConfig outConf;
+				if (ammoDict.TryGetValue (wt.ammoType, out outConf)) {
+					outConf.currentAmmo = ammo;
+					outConf.UpdateAmmoDisplay();
+					return;
+				}
+				Debug.LogError ("Weapon: " + wt.name + " using unlisted ammo type: " + wt.ammoType.ToString ());
 				return;
 			}
 		}
@@ -188,10 +230,48 @@ public class WeaponManager : MonoBehaviour {
 	}
 
 	[System.Serializable]
+	public class AmmoConfig {
+		public AmmoType ammoType;
+		public int currentAmmo;
+		public int maxAmmo;
+		public GUIFontLetter[] ammoDisplay;
+		public GUIFontLetter[] maxDisplay;
+
+		public void UpdateAmmoDisplay() {
+			ammoDisplay [0].gameObject.SetActive (currentAmmo >= 100);
+			ammoDisplay [1].gameObject.SetActive (currentAmmo >= 10);
+			int numberIdx = 0;
+			for (int i = 0; i < 3; i++) {
+				if (ammoDisplay [i].gameObject.activeSelf) {
+					ammoDisplay[i].SetLetter(currentAmmo.ToString()[numberIdx].ToString());
+					numberIdx++;
+				}
+			}
+			maxDisplay [0].gameObject.SetActive (maxAmmo >= 100);
+			maxDisplay [1].gameObject.SetActive (maxAmmo >= 10);
+			numberIdx = 0;
+			for (int i = 0; i < 3; i++) {
+				if (maxDisplay [i].gameObject.activeSelf) {
+					maxDisplay[i].SetLetter(maxAmmo.ToString()[numberIdx].ToString());
+					numberIdx++;
+				}
+			}
+		}
+	}
+
+	public enum AmmoType {
+		BULL,
+		SHEL,
+		RCKT,
+		CELL,
+		UNLM
+	}
+
+	[System.Serializable]
 	public class WeaponType {
 		public string name;
-		public int ammo;
-		public bool unlimitedAmmo;
+		public AmmoType ammoType;
+		//public bool unlimitedAmmo;
 		public int damage = 5;
 		[TooltipAttribute("ID of sprite when idle")]
 		public string idleSprite;
